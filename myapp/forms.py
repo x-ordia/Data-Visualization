e27c8190a647f2ae796468c075fa390ef4e8ceab
Bhargav Bodhankar <89120526+x-ordia@users.noreply.github.com>
2022-06-14 19:41:38 +0530

    Delete forms.py

--- myapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Image
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = '__all__'
-        labels = {'photo':''}
